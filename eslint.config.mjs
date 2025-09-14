019a7c8ecdb5d01a3a4786fdb1472b4bef164dbe
Edgar Tolete <edgartolete@outlook.com>
2025-09-15 01:35:33 +0800

    feat: use Fastify instead of ExpressJS

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+// eslint.config.mjs
+import parser from '@typescript-eslint/parser'
+import plugin from '@typescript-eslint/eslint-plugin'
+import prettier from 'eslint-plugin-prettier'
+
+export default [
+  {
+    ignores: ['dist', 'node_modules'],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: import.meta.dirname,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': plugin,
+      prettier,
+    },
+    rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'prettier/prettier': 'error',
+    },
+  },
+]
+
