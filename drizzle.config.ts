1ff46ac2a65d0071109900aa8a353824c5beedae
Edgar Tolete <edgartolete@outlook.com>
2025-09-21 16:51:32 +0800

    revise config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,12 @@
 import type { Config } from 'drizzle-kit';
-import { env } from './env';
 
 export default {
   schema: './src/db/schema',
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    host: env.DB_HOST,
-    user: "",
+    host: process.env.DB_HOST!,
+    user: process.env.DB_USER!,
     password: process.env.DB_PASSWORD!,
     database: process.env.DB_NAME!,
     port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : undefined,
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,27 +1,27 @@
-import dotenv from 'dotenv'
-import { drizzle } from 'drizzle-orm/mysql2'
-import mysql from 'mysql2/promise'
-import * as userSchema from './schema/users.schema'
-import * as profileSchema from './schema/profiles.schema'
-import * as sessionSchema from './schema/sessions.schema'
-import * as roleSchema from './schema/roles.schema'
-import * as appSchema from './schema/apps.schema'
-import * as actionSchema from './schema/actions.schema'
-import * as groupSchema from './schema/groups.schema'
-import * as resourceSchema from './schema/resources.schema'
-import * as groupRoleSchema from './schema/groupRoles.schema'
-import * as resourceRoleSchema from './schema/resourceRoles.schema'
-import * as resourceRolePermissionschema from './schema/resourceRolePermissions.schema'
+import dotenv from 'dotenv';
+import { drizzle } from 'drizzle-orm/mysql2';
+import * as userSchema from './schema/users.schema';
+import * as profileSchema from './schema/profiles.schema';
+import * as sessionSchema from './schema/sessions.schema';
+import * as roleSchema from './schema/roles.schema';
+import * as appSchema from './schema/apps.schema';
+import * as actionSchema from './schema/actions.schema';
+import * as groupSchema from './schema/groups.schema';
+import * as resourceSchema from './schema/resources.schema';
+import * as groupRoleSchema from './schema/groupRoles.schema';
+import * as resourceRoleSchema from './schema/resourceRoles.schema';
+import * as resourceRolePermissionschema from './schema/resourceRolePermissions.schema';
+import { Pool } from 'pg';
 
-dotenv.config({ quiet: true })
+dotenv.config({ quiet: true });
 
-const poolConnection = mysql.createPool({
+const poolConnection = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : undefined
-})
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : undefined,
+});
 
 export const db = drizzle(poolConnection, {
   schema: {
@@ -35,7 +35,7 @@ export const db = drizzle(poolConnection, {
     ...groupRoleSchema,
     ...resourceRoleSchema,
     ...resourceRolePermissionschema,
-    ...roleSchema
+    ...roleSchema,
   },
-  mode: 'default'
-})
+  mode: 'default',
+});
