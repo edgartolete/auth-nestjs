9e63f8284077ad50f0a813e2a5fbe9308e67f5d5
Edgar Tolete <edgartolete@outlook.com>
2025-09-21 16:01:14 +0800

    enabled global validation tranform from dtos

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,21 @@ import {
 } from '@nestjs/platform-fastify';
 
 import qs from 'qs';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter({
       querystringParser: (str) => qs.parse(str),
+    }),
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
     }),
   );
   await app.listen(3000, '0.0.0.0');
